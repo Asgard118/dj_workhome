55573d45ea2a132fa414acdcd39910596533bb25
ArtemB118 <1evkakii1@gmail.com>
2023-10-25 16:12:38 +0600

    mission 1.2 complete

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.core.paginator import Paginator
+import csv
 
 
 def index(request):
     return redirect(reverse('bus_stations'))
 
-
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
+    with open('data-398-2018-08-30.csv', newline='', encoding='utf-8') as file:
+        reader = csv.DictReader(file)
+        bus_stations_data = list(reader)
+
+    page = int(request.GET.get("page", 1))
+    paginator = Paginator(bus_stations_data, 14)
+    this_page = paginator.get_page(page)
 
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': this_page.object_list,
+        'page': this_page
     }
     return render(request, 'stations/index.html', context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 DATA = {
     'omlet': {
@@ -19,12 +20,24 @@ DATA = {
     # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def omlet(request):
+    servings = int(request.GET["servings"]) if "servings" in request.GET else 1
+    recipe = DATA.get('omlet', {})
+    adjusted_recipe = {key: value * servings for key, value in recipe.items()}
+    response_str = "\n".join([f"{key}: {value}" for key, value in adjusted_recipe.items()])
+    return HttpResponse(response_str, content_type="text/plain; charset=utf-8")
+
+def pasta(request):
+    servings = int(request.GET["servings"]) if "servings" in request.GET else 1
+    recipe = DATA.get('pasta', {})
+    adjusted_recipe = {key: value * servings for key, value in recipe.items()}
+    response_str = "\n".join([f"{key}: {value}" for key, value in adjusted_recipe.items()])
+    return HttpResponse(response_str, content_type="text/plain; charset=utf-8")
+
+def buter(request):
+    servings = int(request.GET["servings"]) if "servings" in request.GET else 1
+    recipe = DATA.get('buter', {})
+    adjusted_recipe = {key: value * servings for key, value in recipe.items()}
+    response_str = "\n".join([f"{key}: {value}" for key, value in adjusted_recipe.items()])
+    return HttpResponse(response_str, content_type="text/plain; charset=utf-8")
