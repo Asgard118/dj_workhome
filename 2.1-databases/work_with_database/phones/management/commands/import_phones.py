b8e035b96072bc4ab0f8982406e9c11502dd607d
ArtemB118 <1evkakii1@gmail.com>
2023-11-01 14:19:24 +0600

    mission 2.1 complete

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,8 +1,6 @@
 import csv
-
 from django.core.management.base import BaseCommand
 from phones.models import Phone
-
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
@@ -12,6 +10,17 @@ class Command(BaseCommand):
         with open('phones.csv', 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
-        for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+        for phone_data in phones:
+            # Создаем новый объект Phone
+            phone = Phone(
+                name=phone_data['name'],
+                price=phone_data['price'],
+                image=phone_data['image'],
+                release_date=phone_data['release_date'],
+                lte_exists=phone_data['lte_exists']
+            )
+            # Сохраняем объект в базе данных
+            phone.save()
+
+        self.stdout.write(self.style.SUCCESS('Телефоны успешно импортированы!'))
+
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,15 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
 
+    sort_by = request.GET.get('sort', 'name')
+    phones = Phone.objects.all().order_by(sort_by)
+
+    context = {'phones': phones}
+    return render(request, template, context)
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = get_object_or_404(Phone, slug=slug)
+    context = {'phone': phone}
     return render(request, template, context)
